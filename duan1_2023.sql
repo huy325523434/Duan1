a03d11b67bf3966fa03f002d8cf53d2b7059062d
hoadtph31026 <119299310+hoadtph31026@users.noreply.github.com>
2023-11-15 20:46:06 +0700

    Add files via upload

---
+++ duan1_2023.sql
@@ -0,0 +1,95 @@
+CREATE TABLE `donhang` (
+  `id_dh` integer PRIMARY KEY,
+  `id_user` integer,
+  `id_sp` integer,
+  `trangthai` varchar(255),
+  `ngaydat` datetime
+);
+
+CREATE TABLE `binhluan` (
+  `id_bl` integer,
+  `noidung` varchar(255),
+  `ngaybl` datetime,
+  `danhgia` integer,
+  `id_sp` integer,
+  `id_user` integer
+);
+
+CREATE TABLE `chitietdonhang` (
+  `id_ctdh` integer PRIMARY KEY,
+  `id_dh` integer,
+  `id_sp` integer,
+  `id_size` integer,
+  `id_mau` integer,
+  `id_img` integer,
+  `thanhtien` integer,
+  `soluong` integer
+);
+
+CREATE TABLE `hoadon` (
+  `id_hd` integer PRIMARY KEY,
+  `id_dh` integer,
+  `pttt` varchar(255),
+  `tongtien` integer,
+  `ngaythanhtoan` datetime
+);
+
+CREATE TABLE `chitiethoadon` (
+  `id_cthd` integer PRIMARY KEY,
+  `id_hd` integer,
+  `id_sp` integer,
+  `id_size` integer,
+  `id_mau` integer,
+  `id_img` integer,
+  `price` integer,
+  `soluong` integer
+);
+
+CREATE TABLE `danhmuc` (
+  `id_dm` integer PRIMARY KEY,
+  `name` varchar(255)
+);
+
+CREATE TABLE `sanpham` (
+  `id_sp` integer PRIMARY KEY,
+  `name` varchar(255),
+  `original_price` integer,
+  `mota` text,
+  `trangthai` integer,
+  `id_dm` integer,
+  `id_size` integer,
+  `id_mau` integer,
+  `id_img` integer,
+  `price` integer
+);
+
+CREATE TABLE `img` (
+  `id_img` integer PRIMARY KEY,
+  `name` varchar(255)
+);
+
+CREATE TABLE `taikhoan` (
+  `id_user` integer PRIMARY KEY,
+  `user` varchar(255),
+  `pass` varchar(255),
+  `email` varchar(255),
+  `address` varchar(255),
+  `tel` varchar(255),
+  `role` integer
+);
+
+CREATE TABLE `size` (
+  `id_size` integer PRIMARY KEY,
+  `name` varchar(255)
+);
+
+CREATE TABLE `mau` (
+  `id_mau` integer PRIMARY KEY,
+  `name` varchar(255)
+);
+
+CREATE TABLE `chitietsp` (
+  `id_ctsp` integer PRIMARY KEY,
+  `id_sp` integer,
+  `soluong` integer
+);
